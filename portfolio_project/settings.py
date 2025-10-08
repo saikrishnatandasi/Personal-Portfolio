19da0a305d4765d0a0df299ab9b94a8c86fef367
Sai Krishna Tandasi <160542032+saikrishnatandasi@users.noreply.github.com>
2025-10-08 12:04:36 +0530

    Update settings.py

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7zpa)s%3rtmh944ifejyk1t-x1hh72z%-ce(j@6tvo^u!d^*_-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','personal-portfolio-v13k.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','personal-portfolio-v13k.onrender.com','personal-portfolio-main-a32dd53.kuberns.cloud']
 
 
 # Application definition
